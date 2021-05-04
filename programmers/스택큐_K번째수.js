dc47a2dcf572094461d09467ae00312fc508b4cf
Park-SM <park97.sm@gmail.com>
2021-05-04 17:02:27 +0900

    스택큐_K번째수 문제 풀이

---
+++ programmers/스택큐_K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    var answer = [];
+    for (var i = 0; i < commands.length; i++) {
+        const arr = array.slice(commands[i][0] - 1, commands[i][1]);
+        answer.push(arr.sort( (a, b) => { return a - b; })[commands[i][2] - 1]);
+    }
+    return answer;
+}
