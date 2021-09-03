4d46fb4495d5b3c235e80e64a81d7362fc786c7d
ParkSM <park97.sm@gmail.com>
2021-09-03 18:21:21 +0900

    [KAKAO 인턴십]보석쇼핑 주석 추가

--- programmers/[KAKAO 인턴십] 보석쇼핑.kt
+++ programmers/[KAKAO 인턴십] 보석쇼핑.kt
@@ -1,5 +1,11 @@
 class Solution {
 
+    /* Note:
+     * List를 Map으로 변환할 때 List를 forEach로 순회하며 Map에 put 하는 것보다
+     * List를 toHashSet()을 통해 Set으로 변환 후 Map에 put 하는 것이 빠르다.
+     *
+     * List를 forEach로 순회하며 Map에 put 하면 효율성에서 오답처리 됨.
+     */
     fun solution(gems: Array<String>): IntArray {
         val answer = IntArray(2)
         val types = LinkedHashMap<String, Int>()
